a8db3c44fca6c24b4b2c944790a0a758057f2b7f
kr-mustali <mustali.c@knackroot.com>
2024-01-30 17:26:52 +0530

    ecomm app complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import ProductProvider from "./context.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ProductProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProductProvider>
+);
